366d9089204bc036bb192479b4670b9cbebc1194
Victor Oberti <victor.oberti.vo@gmail.com>
2026-03-25 23:50:46 +0100

    first commit

---
+++ webaz-docker-starter-kit/apache-php/src/views/accueil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://unpkg.com/ol/ol.css">
+    <script src="https://unpkg.com/ol/dist/ol.js"></script>
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+
+<div id="entete" >
+    <p> {{toponyme}} </p>
+</div>
+
+<div id = "map" @click ="click"></div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="/assets/ol.js"></script>
+
+</body>
+
+
+
+
+
+
+
+</html>
