288c82bc3391d7162a9739a0fa7a3963fbe002d2
Victor Oberti <victor.oberti.vo@gmail.com>
2026-03-27 14:11:09 +0100

    test database

--- webaz-docker-starter-kit/apache-php/src/index.php
+++ webaz-docker-starter-kit/apache-php/src/index.php
@@ -4,24 +4,40 @@ declare(strict_types=1);
 
 require_once 'flight/Flight.php';
 
+
+$link = mysqli_connect("mysql", "root", "root", "geobase", 3306);
+mysqli_set_charset($link, "utf8");  
+
+// stocker une variable globale
+Flight::set('geobase', $link);
+// récupérer la variable
+Flight::get('geobase');
+
 Flight::route('/', function() {
     Flight::render('accueil');
 });
 
+Flight::route('/info', function() {
+    Flight::render('info');
+});
+
 Flight::route('/test-db', function () {
-    $host = 'db';
-    $port = 5432;
-    $dbname = 'mydb';
-    $user = 'postgres';
-    $pass = 'postgres';
-
-    // Connexion BDD
-    $link = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
-
-    $sql = "SELECT * FROM points";
-    $query = pg_query($link, $sql);
-    $results = pg_fetch_all($query);
+    $sql = "SELECT nom FROM communes";
+    $query = mysqli_query(Flight::get('geobase'), $sql);
+    $results = mysqli_fetch_all($query, MYSQLI_ASSOC);
     Flight::json($results);
+});
+
+
+Flight::route('/regions', function(){
+    $sql = "SELECT nom FROM regions";
+    $regions = mysqli_query(Flight::get('geobase'),$sql);
+    $regions = mysqli_fetch_all($regions, MYSQLI_ASSOC);
+    Flight::json($regions);
+});
+
+FLIGHT::route('/test', function() {
+    Flight::render('test');
 });
 
 Flight::start();
